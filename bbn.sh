1b3339a828d8c4e2f49ec5d249ff85ea579b696c
oekshido <joseugarte2262005@gmail.com>
2020-08-29 13:55:42 -0400

    Removed 'if's in favour of operators.

--- bbn.sh
+++ bbn.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-if [ -z $NOTES_DIR ] || [ ! -d $NOTES_DIR ]; then
-	echo "NOTES_DIR enviroment variable doesn't exist or is not a directory."
-	exit 1
-fi
+[ -z $NOTES_DIR ] && [ ! -d $NOTES_DIR ] && echo "NOTES_DIR enviroment variable doesn't exist or is not a directory." && exit 1
 
-if [ -z $1 ]; then # Default journal
-	NOTE="main"
-else
-	NOTE=$1
-fi
+[ -z $1 ] && NOTE="main" || NOTE=$1
 
 TEMP_FILE="${NOTES_DIR}/temp.file"
 FILE="${NOTES_DIR}/${NOTE}.md"
@@ -17,11 +10,7 @@ DATE="$(date +'%D (%A)')"
 
 touch $FILE
 
-if grep -q "$DATE" $FILE; then # If curret date is in file.
-	DATE=""
-else
-	DATE="\n# ${DATE}\n\n"
-fi
+grep -q "$DATE" $FILE && DATE="" || DATE="\n# ${DATE}\n\n"
 
 nano $TEMP_FILE -t -x # Arguments hide shortcuts and save without asking.
 
